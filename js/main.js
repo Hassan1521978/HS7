402ab0b01dc8b28ee925fbce19032a6d3516e48f
Hassan1521978 <sona1521978@gmail.com>
2025-05-04 21:22:39 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// document.getElementById('demo').innerHTML = "HELLO WORLED FROM JS CODE";
+
+// document.getElementById('demo').style.backgroundColor = "red";
+
+// document.getElementById('demo').style.marginTop = "50px";
+
+// document.getElementById('demo').style.textAlign = "center";
