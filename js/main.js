2d47c788fbe09a2b908ac1e308f2ab46fb610ad9
Hassan1521978 <sona1521978@gmail.com>
2025-05-16 21:40:39 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
-// document.getElementById('demo').innerHTML = "HELLO WORLED FROM JS CODE";
+// Validate Data function
 
-// document.getElementById('demo').style.backgroundColor = "red";
+function validateData(){
+    var user = document.getElementById("user").value;
+    var email = document.getElementById("email").value;
+    var pass = document.getElementById("pass").value;
+    var confirm = document.getElementById("confirm").value;
+    var result = document.getElementById("result");
 
-// document.getElementById('demo').style.marginTop = "50px";
+    result.setAttribute("class","alert alert-danger text-center resulto");
 
-// document.getElementById('demo').style.textAlign = "center";
+    if(user=="" && email=="" && pass=="" && confirm==""){
+        result.innerHTML = "Enter Data In Form";
+        return false;
+    }else if(user.length<5 || user.length>15){
+        result.innerHTML = "Enter in Name 5-15 chracter";
+        return false;
+    }else if(email.indexOf("@",3) ==-1 || email.indexOf("gmail.com") ==-1){
+        result.innerHTML = "Enter valid Email";
+        return false;
+    }else if(pass==""){
+        result.innerHTML = "Enter Password";
+        return false;
+    }else if(pass.length<8){
+        result.innerHTML = "Week Password";
+        return false;
+    }else if(pass != confirm){
+        result.innerHTML = "Mathed Password";
+        return false;
+    }
+}
