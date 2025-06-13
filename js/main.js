2c213ce1d2772185b8cd791c02f0c1f47e4cfc20
Hassan1521978 <sona1521978@gmail.com>
2025-06-13 19:15:41 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,31 +1,11 @@
-// Validate Data function
+document.querySelector("#red").onclick = function(){
+    document.querySelector("#demo").style.color = "red";
+}
 
-function validateData(){
-    var user = document.getElementById("user").value;
-    var email = document.getElementById("email").value;
-    var pass = document.getElementById("pass").value;
-    var confirm = document.getElementById("confirm").value;
-    var result = document.getElementById("result");
+document.querySelector("#yellow").onclick = function(){
+    document.querySelector("#demo").style.color = "yellow";
+}
 
-    result.setAttribute("class","alert alert-danger text-center resulto");
-
-    if(user=="" && email=="" && pass=="" && confirm==""){
-        result.innerHTML = "Enter Data In Form";
-        return false;
-    }else if(user.length<5 || user.length>15){
-        result.innerHTML = "Enter in Name 5-15 chracter";
-        return false;
-    }else if(email.indexOf("@",3) ==-1 || email.indexOf("gmail.com") ==-1){
-        result.innerHTML = "Enter valid Email";
-        return false;
-    }else if(pass==""){
-        result.innerHTML = "Enter Password";
-        return false;
-    }else if(pass.length<8){
-        result.innerHTML = "Week Password";
-        return false;
-    }else if(pass != confirm){
-        result.innerHTML = "Mathed Password";
-        return false;
-    }
+document.querySelector("#green").onclick = function(){
+    document.querySelector("#demo").style.color = "green";
 }
