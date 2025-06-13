b3029b5155a4e7062c8ef8b1ff03565e779416f7
Hassan1521978 <sona1521978@gmail.com>
2025-06-13 20:32:33 +0300

    Add files via upload
    Signed-off-by: Hassan1521978 <sona1521978@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-document.querySelector("#red").onclick = function(){
-    document.querySelector("#demo").style.color = "red";
-}
+// document.querySelector("#red").onclick = function(){
+//     document.querySelector("#demo").style.color = "red";
+// }
 
-document.querySelector("#yellow").onclick = function(){
-    document.querySelector("#demo").style.color = "yellow";
-}
+// document.querySelector("#yellow").onclick = function(){
+//     document.querySelector("#demo").style.color = "yellow";
+// }
 
-document.querySelector("#green").onclick = function(){
-    document.querySelector("#demo").style.color = "green";
-}
+// document.querySelector("#green").onclick = function(){
+//     document.querySelector("#demo").style.color = "green";
+// }
+
+// ========================================================
+// ========================================================
+// ========================================================
+
+
+document.querySelectorAll(".pen").forEach( function(item){
+
+    item.onclick = function(){
+        document.querySelector("#demo").style.color = item.dataset.color;
+        document.querySelector("#demo").style.textAlign = item.dataset.align;
+    }
+
+});
