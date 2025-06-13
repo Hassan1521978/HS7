674d806cc4d6ff232ce9e1ef7eb89bcd21b67859
Hassan1521978 <sona1521978@gmail.com>
2025-06-13 22:54:14 +0300

    Add files via upload
    Signed-off-by: Hassan1521978 <sona1521978@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
-// document.querySelector("#red").onclick = function(){
-//     document.querySelector("#demo").style.color = "red";
-// }
+document.querySelector("#form-vaild").addEventListener("submit",function(event){
+    var works = document.querySelector("#works").value;
+    var result = document.querySelector("#result");
 
-// document.querySelector("#yellow").onclick = function(){
-//     document.querySelector("#demo").style.color = "yellow";
-// }
-
-// document.querySelector("#green").onclick = function(){
-//     document.querySelector("#demo").style.color = "green";
-// }
-
-// ========================================================
-// ========================================================
-// ========================================================
-
-
-document.querySelectorAll(".pen").forEach( function(item){
-
-    item.onclick = function(){
-        document.querySelector("#demo").style.color = item.dataset.color;
-        document.querySelector("#demo").style.textAlign = item.dataset.align;
+    if(works==""){
+        alert("Enter Value");
+        event.preventDefault();
+    }else{
+        var myLi = document.createElement("li");
+        myLi.innerHTML = works;
+        result.append(myLi);
+        document.querySelector("#works").value ='';
+        event.preventDefault();
+        
     }
 
-});
+})
